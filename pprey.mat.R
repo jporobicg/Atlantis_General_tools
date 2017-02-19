af4661634472b7514b8d403920fc6279d9da8ed2
jporobicg <jporobicg@gmail.com>
2017-02-20 09:51:33 +1100

    Fixed libraries

--- pprey.mat.R
+++ pprey.mat.R
@@ -11,7 +11,7 @@ feeding.mat.shy <- function(prm.file, grp.file, nc.file){
     ## I need to create something more elegant for this
     library(shiny)
     library(ncdf4)
-    library(tidyverser)
+    library(tidyverse)
     ## Reading files
     groups.csv <- read.csv(grp.file)
     prm        <- readLines(prm.file, warn = FALSE)
